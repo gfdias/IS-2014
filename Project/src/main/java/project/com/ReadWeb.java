eb8b2082bf31700a34da4820527e403a6bcfbc04
jsaguiar <jsaguiar@me.com>
2014-10-07 20:40:15 +0100

    start header parse

--- Project/src/main/java/project/com/ReadWeb.java
+++ Project/src/main/java/project/com/ReadWeb.java
@@ -1,24 +1,33 @@
 package project.com;
 
-import java.io.IOException;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class ReadWeb {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Start Read Web");
 		ReadWeb a=new ReadWeb();
-		a.getFromWeb("");
+		
+		a.getFromWeb("http://cnn.com/");
+		System.out.println("End Read Web");
 	}
 	
-	public void getFromWeb(String web){
-		Document doc = Jsoup.connect("http://cnn.com/").get();
-		Elements newsHeadlines = doc.select("#mp-itn b a");
-		System.out.println(newsHeadlines.toString());
+	public void getFromWeb(String webSite) {
+		try {
+			Document doc = Jsoup.connect(webSite).get();
+			Elements newsHeadlines = doc.select("#intl-menu a");
 
-		
-		
+			for (int i = 0; i < newsHeadlines.size(); i++) {
+				System.out.println("/t header"+newsHeadlines.toString());
+				Element header=  newsHeadlines.get(i);
+		        System.out.println(header.tagName("href"));        
+			}
+	       
+		} catch (Exception e) {
+			System.out.println("CANNOT GET HEADERS");
+		}
 	}
 	
 }
