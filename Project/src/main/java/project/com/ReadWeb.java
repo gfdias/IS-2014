3bc2852baa34f45693a0c4acfff75f14a1b429d5
jsaguiar <jsaguiar@me.com>
2014-10-07 23:09:45 +0100

    bug fixe

--- Project/src/main/java/project/com/ReadWeb.java
+++ Project/src/main/java/project/com/ReadWeb.java
@@ -1,16 +1,25 @@
 package project.com;
 
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class ReadWeb {
+	ArrayList<String> ignore = new ArrayList<String>() {{
+	    add("video");
+	    add("tech");
+	    add("ireport");
+	    add("travel");
+	}};	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Start Read Web");
 		ReadWeb a=new ReadWeb();
 		
-		a.getFromWeb("http://cnn.com");
+		a.getFromWeb("http://edition.cnn.com");
 		System.out.println("End Read Web");
 	}
 	
@@ -24,10 +33,13 @@ public class ReadWeb {
 				Element header=  newsHeadlines.get(i);
 				
 		        String getInfo=header.attr(("href").toString());   
-		        if	(getInfo.equals("/video/")) continue;
+		        if	(getInfo.equals("/video/") || getInfo.equals("/SHOWBIZ/")) continue;
 		        String headerName=header.text();
+		        if (ignore.contains(headerName.toLowerCase())) continue;
 		        System.out.println("Getting news of "+headerName);
 		        String url=webSite+getInfo;
+		        System.out.println("url "+url);
+
 		        TopicNews aux=new  TopicNews(url,headerName);
 		        aux.fetchLatestNews();
 		        
