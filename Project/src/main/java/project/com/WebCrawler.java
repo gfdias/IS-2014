fcbda0fb0f5eb1bafd6cd9f44d81cbbb254fceab
Gonçalo Dias <goncalodias@goncalos-mbp.lan>
2014-10-07 15:34:17 +0100

    teste

--- Project/src/main/java/project/com/WebCrawler.java
+++ Project/src/main/java/project/com/WebCrawler.java
@@ -4,7 +4,7 @@ public class WebCrawler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ES MESMO GAY");
+		System.out.println("void name(){}");
 	}
 
 }
