209f80ae20734528587a1bbc0b09e0d2475b2244
jsaguiar <jsaguiar@me.com>
2014-10-14 02:48:17 +0100

    jms integration

---
+++ Project/src/main/java/sender/Receiver.java
@@ -0,0 +1,53 @@
+package sender;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class Receiver {
+ private ConnectionFactory cf;
+ private Connection c;
+ private Session s;
+ private Destination d;
+ private MessageConsumer mc;
+
+ public Receiver() throws NamingException, JMSException {
+  InitialContext init = new InitialContext();
+  this.cf = (ConnectionFactory) init.lookup("jms/RemoteConnectionFactory");
+  this.d = (Destination) init.lookup("jms/queue/PlayQueue");
+  this.c = (Connection) this.cf.createConnection("joao", "passwd");
+  this.c.start();
+  this.s = this.c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+  mc = s.createConsumer(d);
+ }
+
+ private String receive() throws JMSException {
+  TextMessage msg = (TextMessage) mc.receive();
+  return msg.getText();
+ }
+
+ private void close() throws JMSException {
+  this.c.close();
+ }
+
+ /**
+  * @param args
+  * @throws JMSException 
+  * @throws NamingException 
+  */
+ public static void main(String[] args) throws NamingException, JMSException {
+  Receiver r = new Receiver();
+
+  String msg = r.receive();
+  System.out.println("Mensagem: " + msg);
+  r.close();
+ }
+
+}
---
+++ Project/src/main/java/sender/Sender.java
@@ -0,0 +1,56 @@
+package sender;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.CommunicationException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class Sender {
+	private ConnectionFactory cf;
+	private Connection c;
+	private Session s;
+	private Destination d;
+	private MessageProducer mp;
+
+	public Sender() throws NamingException, JMSException {
+		InitialContext init = new InitialContext();
+		this.cf = (ConnectionFactory) init
+				.lookup("jms/RemoteConnectionFactory");
+		this.d = (Destination) init.lookup("jms/queue/PlayQueue");
+		this.c = (Connection) this.cf.createConnection("joao", "passwd");
+		this.c.start();
+		this.s = this.c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		this.mp = this.s.createProducer(this.d);
+	}
+
+	private void send(String string) throws JMSException {
+		TextMessage tm = this.s.createTextMessage(string);
+		this.mp.send(tm);
+	}
+
+	private void close() throws JMSException {
+		this.c.close();
+	}
+
+	/**
+	 * @param args
+	 * @throws JMSException
+	 * @throws NamingException
+	 */
+	public static void main(String[] args) throws NamingException, JMSException{
+		Sender s;
+		try {
+			s = new Sender();
+			s.send("Ola ca estou eu!");
+			s.close();
+		}catch (CommunicationException a){
+			System.out.println("JMS IS DOWN");
+		}
+	}
+}
